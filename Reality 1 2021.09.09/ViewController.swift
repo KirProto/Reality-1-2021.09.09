f5c769a70f492751c2a5423cb1483177a45958c7
Kir Proto Q <kir.proto@ya.ru>
2021-09-09 17:30:37 +0500

    Initial Commit

---
+++ Reality 1 2021.09.09/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  Reality 1 2021.09.09
+//
+//  Created by KIR Q on 2021.09.09.
+//
+
+import UIKit
+import RealityKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet var arView: ARView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Load the "Box" scene from the "Experience" Reality File
+        let boxAnchor = try! Experience.loadBox()
+        
+        // Add the box anchor to the scene
+        arView.scene.anchors.append(boxAnchor)
+    }
+}
